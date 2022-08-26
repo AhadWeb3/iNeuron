6702bf02e1e32d197e6d5beef1db4b00be5cfac1
Abdul Ahad <ahadweb3.0@gmail.com>
2022-08-26 15:23:19 +0530

    added readline

--- Assignment1/factorial.js
+++ Assignment1/factorial.js
@@ -1,12 +1,33 @@
 // formula for a facrorial is n*(n-1)
 
+// let factorial = function(n){
+//     if(n === 0){ return 1;}
+//     else{ return n * (n-1) }
+// }
 
+// console.log(factorial(3));
 
+// const input = prompt('Please enter a number');
+console.log("this is a log");
 
-let factorial = function(n){
-    if(n === 0){ return 1;}
-    else{ return n * (n-1) }
-}
+// import readline module
+const readline = require("readline");
 
-console.log(factorial(3));
+// create interface for input and output
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// create empty user input
+let userInput = "";
+// question user to enter name
+rl.question("What is your name\n", function (string) {
+    userInput = string;
+  
+    console.log("Your name is " + userInput);
+  
+    // close input stream
+    rl.close();
+  });
 
