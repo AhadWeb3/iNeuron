0535310ea88f3213921cef68772e873ac745eddf
Abdul Ahad <ahadweb3.0@gmail.com>
2022-08-26 16:12:51 +0530

    addition using function

--- Assignment1/factorial.js
+++ Assignment1/factorial.js
@@ -1,10 +1,18 @@
 const prompt = require("prompt-sync")();
-// store input numbers
+
 const num1 = parseInt(prompt('Enter the first number '));
 const num2 = parseInt(prompt('Enter the second number '));
 
-//add two numbers
-const sum = num1 + num2;
+function add(num1, num2) {
+    let sum = num1 + num2;
+    console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+}
 
-// display the sum
-console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+add(num1, num2);
+
+
+
+
+/* 
+factorial of a number
+*/
