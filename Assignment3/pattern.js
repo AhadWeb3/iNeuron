f612ad47b8bbeaa336efc6e8354fb1b2b8273211
Abdul Ahad <ahadweb3.0@gmail.com>
2022-08-27 11:56:24 +0530

    pattern

--- Assignment3/pattern.js
+++ Assignment3/pattern.js
@@ -13,22 +13,3 @@ for numbers its just for(i=1,i<=10,i++); console.log(i);
 //     if(i<=6 && i>3){console.log(i);}
 //     if(i<=10 && i>6){console.log(i);}
 // }
-
-let n = rows = 5;
-let m = columns = 5;
-string = "";
-for (let j = 0; j < n; j++) {                                  //number of rows
-  for (let i = 0; i < m; i++) {  // number of stars in single row
-    if (j === 0 || j === m - 1) {
-      string += "*";
-    } else {
-      if (i === 0 || i === n - 1) {
-        string += "*";
-      } else {
-        string += " ";
-      }
-    }
-    string = string + "\n";
-  }
-}
-console.log(string);
